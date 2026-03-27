d6c5c918ec66ec797d254f3ae89e84e12aea364e
Hardik Kotadiya <hardikkotadiya90@gmail.com>
2026-03-27 17:20:26 +0530

    Change ad sizes

--- src/lib/ads.ts
+++ src/lib/ads.ts
@@ -12,7 +12,7 @@ export const AD_UNITS = {
   'result-top': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1773233679776-0',
-    sizes: [[320, 100], [300, 250]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'result-bottom': {
     path: '/23314720614/new-test-wallpaper-top-ads',
@@ -23,32 +23,32 @@ export const AD_UNITS = {
   'blog-list-top': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1773233767926-0',
-    sizes: [[320, 100], [320, 50], [300, 50]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'blog-list-center': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1773233776752-0',
-    sizes: [[300, 250], [336, 280]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'article-top': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1773233787861-0',
-    sizes: [[320, 100], [320, 50]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'article-center': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1773233841663-0',
-    sizes: [[300, 250], [336, 280]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'quiz-top': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1771002656033-0',
-    sizes: [[320, 50], [320, 100]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'quiz-center': {
     path: '/23314720614/new-test-14-12-2025-ads-unit-for-job',
     id: 'div-gpt-ad-1771002813935-0',
-    sizes: [[300, 250]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'quiz-bottom': {
     path: '/23314720614/new-test-wallpaper-top-ads',
@@ -58,12 +58,12 @@ export const AD_UNITS = {
   'wallet-top': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1773233810058-0',
-    sizes: [[320, 100], [320, 50]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'wallet-center': {
     path: '/23314720614/new-test-wallpaper-top-ads',
     id: 'div-gpt-ad-1773233820105-0',
-    sizes: [[300, 250], [336, 280]]
+    sizes: [[320, 100], [336, 280], [300, 100], [300, 250], [300, 50], [320, 50]]
   },
   'wallet-bottom': {
     path: '/23314720614/new-test-wallpaper-top-ads',
@@ -73,7 +73,7 @@ export const AD_UNITS = {
   'sidebar-big': {
     path: '/23314720614/header-1',
     id: 'div-gpt-ad-1773234063062-0',
-    sizes: [[300, 600], 'fluid']
+    sizes: [[300, 600], [300, 250], [336, 280], 'fluid']
   },
   'mobile-anchor': {
     path: '/23314720614/sticky_mobile_banner',
